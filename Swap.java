bce363ea7d8d7dc1d7d485b5ffd84fa5d70a5a2a
aravindhrajaram <50267914+aravindhrajaram@users.noreply.github.com>
2019-05-13 20:34:41 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,16 @@
+class Swap{
+public static void main(String[] args) {
+		
+	int a,b,c,d;
+	 a=10;
+	 b=20;
+	 c=30;
+	 System.out.println("Before swap:\na="+a+"\nb="+b+"\nc="+c);
+	 d=a;
+	 a=b;
+	 b=c;
+	 c=a;
+	 System.out.println("After swap:\na="+a+"\nb="+b+"\nc="+c+"\nd="+d);
+	}
+}
+
