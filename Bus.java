07a99f4d72f60d2d11f50bfd8e2f48d8dd5f043c
aravindhrajaram <50267914+aravindhrajaram@users.noreply.github.com>
2019-05-13 20:36:41 +0530

    Add files via upload
    Addition using object

---
+++ Bus.java
@@ -0,0 +1,14 @@
+class Bus{
+	
+	public static void main(String[] a) {
+		Bus driver=new Bus();
+		driver.add(10,20);
+	}
+   void add(int a,int b){
+ 
+ int c=a+b;
+ System.out.println("The result is"+c  );
+ 
+
+}
+}
