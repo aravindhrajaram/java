af43f46467d8a3b2903bd3515f4339ea6b00b2e1
aravindhrajaram <50267914+aravindhrajaram@users.noreply.github.com>
2019-05-14 19:49:32 +0530

    Odd or Even

---
+++ Program2.java
@@ -0,0 +1,13 @@
+class Program2{
+	public static void main(String[] args) {
+		int a=-11;
+		if(a%2==0&&a>0){
+			System.out.println("even");}
+			else if(a%2==1){
+				System.out.println("odd");
+			}
+			else{
+				System.out.println("negative");
+			}
+		}
+	}
