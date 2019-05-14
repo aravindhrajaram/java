a7e17e43bd7b94c888d53981c2bd08c4d5d4ee49
aravindhrajaram <50267914+aravindhrajaram@users.noreply.github.com>
2019-05-14 19:48:17 +0530

    Add files via upload
    Bing Bang

---
+++ Program.java
@@ -0,0 +1,20 @@
+class Program{
+	public static void main(String[] args){
+		int a=15;
+		
+		 if (a%3==0&&a%5==0)
+{System.out.println("Bing bang");}
+		else if(a%3==0)
+{System.out.println("bing");}
+        else if(a%5==0)
+{System.out.println("bang");}
+
+else
+{
+	System.out.println("Not divisible"+a);
+}
+	
+
+	}
+	
+}
