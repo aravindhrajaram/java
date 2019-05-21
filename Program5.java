3d8768a4b174684f655572d21eb3c69315d44ce8
aravindhrajaram <50267914+aravindhrajaram@users.noreply.github.com>
2019-05-21 19:09:37 +0530

    Pattern

---
+++ Program5.java
@@ -0,0 +1,16 @@
+class Program5
+{
+	public static void main(String arg[])
+	{
+		for(int row=1;row<=3;row++)
+		{
+           for(int col=1;col<=row;col++)
+           {
+           	System.out.print(col);
+           }
+           System.out.println();
+		}
+         
+			
+	}
+}
