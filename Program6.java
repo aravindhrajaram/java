ca52f090998c65ea259c68644cd959e00b6d729f
aravindhrajaram <50267914+aravindhrajaram@users.noreply.github.com>
2019-05-21 19:10:51 +0530

    Alternate number

---
+++ Program6.java
@@ -0,0 +1,15 @@
+class Program6{
+	static public void main(String[] args)
+	 { int i=10;
+		for(int n=1;n<=5;n++)
+		{System.out.println(n);
+
+		 i=i-1;
+		 if(i!=n)
+		 	
+		 System.out.println(i);
+
+		}
+		
+	}
+}
